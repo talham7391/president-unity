2e489a986f782e126bfe0ac43ae3d6bb10d87e59
U-Talha-PC\Talha <talham7391@hotmail.com>
2015-08-25 23:56:22 -0400

    Started working on Main menu + built to android

---
+++ Assets/Scripts/UI/SCBackground.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCBackground : MonoBehaviour {
+
+	public GUIStyle selectButtonStyle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		float buttonSize = 170;
+		if(GUI.Button(new Rect((Screen.width - buttonSize) / 2, (Screen.height - buttonSize) / 2 - 11, buttonSize, buttonSize), "Testing", selectButtonStyle)){
+			Debug.Log("Testing");
+		}
+	}
+}
